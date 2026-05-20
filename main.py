5446700d18579d1e611fa5ef5f162e4c38c8f1a0
Anirudh <aani.spark@gmail.com>
2026-05-20 15:04:31 +0530

    finished resource sufficient feature

--- main.py
+++ main.py
@@ -3,16 +3,26 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-coffeeMachine = "on"
-coffee = Menu()
-reports = CoffeeMaker()
 
 
-while coffeeMachine == "on":
+is_on = True
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
-    choice = input(f"What would you like? {coffee.get_items()}: ").lower()
 
+while is_on:
+    choice = input(f"What would you like? {menu.get_items()}: ").lower()
     if choice == "off":
-        coffeeMachine = "off"
-    if choice == "report":
-        print(reports.report())
+        is_on = False
+    elif choice == "report":
+        print(coffee_maker.report())
+        print(money_machine.report())
+    else:
+        drink = (menu.find_drink(choice))
+        if coffee_maker.is_resource_sufficient(drink):
+            print(money_machine.make_payment(drink.cost))
+
+
+
+
