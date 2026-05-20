4a2cb10d68fa9df9fb0b24501b83dbbe5d193f59
Anirudh <aani.spark@gmail.com>
2026-05-20 15:15:34 +0530

    completed all the features required for the coffee machine

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 
-
-
 is_on = True
 menu = Menu()
 coffee_maker = CoffeeMaker()
@@ -16,12 +14,13 @@ while is_on:
     if choice == "off":
         is_on = False
     elif choice == "report":
-        print(coffee_maker.report())
-        print(money_machine.report())
+        coffee_maker.report()
+        money_machine.report()
     else:
         drink = (menu.find_drink(choice))
         if coffee_maker.is_resource_sufficient(drink):
-            print(money_machine.make_payment(drink.cost))
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
 
 
