8e64a4aaf4e28db7f873e87dfaa10e103c7a9939
Anirudh <aani.spark@gmail.com>
2026-05-20 13:18:02 +0530

    adding PrettyTable interpreter and tested

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("github")
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikache", "Squirtle", "Charmander"] )
+table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
+print(table)
