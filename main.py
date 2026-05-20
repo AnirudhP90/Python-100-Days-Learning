76fcd8c3537f73d79ea2f9059a00a46a2cf38db4
Anirudh <aani.spark@gmail.com>
2026-05-20 14:30:41 +0530

    coffee machine with Report feature completed

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# from turtle import Turtle, Screen
-#
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("red")
-# timmy.forward(100)
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikache", "Squirtle", "Charmander"] )
-table.add_column("Type", ["Electric", "Water", "Fire"])
-table.align = "l"
-print(table)
+
+coffeeMachine = "on"
+coffee = Menu()
+reports = CoffeeMaker()
+
+
+while coffeeMachine == "on":
+
+    choice = input(f"What would you like? {coffee.get_items()}: ").lower()
+
+    if choice == "off":
+        coffeeMachine = "off"
+    if choice == "report":
+        print(reports.report())
